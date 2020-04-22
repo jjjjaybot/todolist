a86bd9d080b9d8553ba133751cccfd5546d8f388
Jiazi Cai <caijiazi777@gmail.com>
2020-04-22 13:33:29 -0400

    Updates

---
+++ precache-manifest.8535c526a6a5d7a9574022918404fa42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fa11b13848440764fa62a90e21cf850",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "ab6a83a40542443c3228",
+    "url": "/todolist/static/css/main.84dc637c.chunk.css"
+  },
+  {
+    "revision": "bdfa3f5b6dc500caa2eb",
+    "url": "/todolist/static/js/2.54666dd1.chunk.js"
+  },
+  {
+    "revision": "78aa0676509153305bde072ed1e9cb1c",
+    "url": "/todolist/static/js/2.54666dd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab6a83a40542443c3228",
+    "url": "/todolist/static/js/main.a74be992.chunk.js"
+  },
+  {
+    "revision": "b97d7cb31fa7e5a5931e",
+    "url": "/todolist/static/js/runtime-main.811a1a38.js"
+  }
+]);
